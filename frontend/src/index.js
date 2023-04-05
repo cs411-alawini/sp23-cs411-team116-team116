41f3bcf742ba1eb756d6c98323773358c80c94d8
whhuang4 <123702462+whhuang4@users.noreply.github.com>
2023-04-05 18:11:34 +0000

    recover incorrect fronend entry point

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-const express = require("express");
-const app = express();
-const mysql = require("mysql");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
