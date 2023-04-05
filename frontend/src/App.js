ae1210ace2cf80f2c7bd9b53c271a7286172709a
tyseto2 <tyseto2@illinois.edu>
2023-04-05 12:42:40 -0500

    started front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import React, {useState, useEffect} from "react";
+import Axios from 'axios';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+
+      <div className = 'form'>
+        <label>Status:</label>
+        <input type="text" name="status" />
+        <label>Status Description:</label>
+        <input type="text" name="status desc" />
+
+        <button>INSERT</button>
+      </div>
+
     </div>
   );
 }
