67bce92d9d32afea242191bfa36d71b6d7405275
whhuang4 <123702462+whhuang4@users.noreply.github.com>
2023-04-05 18:08:38 +0000

    recover incorrect frontend entry points

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
 
-var db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'mypassword',
-    database: 'cs411crimedata',
-})
-
-app.get('/', (require, response) => {
-    const sqlInsert = "INSERT INTO 'Status' ('DR_NO', 'Status', 'Status Desc') VALUES (12345, 'hi', 'random')"
-    db.query(sqlInsert, (err,result) => {
-        response.send("Hello world");
-    })
-})
-
-app.listen(3002, () => {
-    console.log("running on port 3002");
-})
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
