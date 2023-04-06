a6615c59a4c4561b678d7cf9deb84b67174da0b1
tyseto2 <tyseto2@illinois.edu>
2023-04-06 17:08:00 -0500

    idk

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,21 @@ const mysql = require("mysql");
 const cors = require("cors");
 
 // db connection, fill in required configs
-// var db = mysql.createConnection({
-//  host:'localhost',
-//  user: 'root',
-//  password:'mypassword',
-//  database:'411demo',
-// })
+var db = mysql.createConnection({
+ host:'34.27.148.60',
+ user: 'root',
+ password:'1234',
+ database:'cs411crime',
+})
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database successfully!');
+});
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
@@ -25,6 +34,58 @@ app.use(express.json());
 //     console.log(error);
 //     })
 //    });
+
+
+// app.get('/', (require,response) => {
+//     const sqlInsert = 'INSERT INTO `Status` (`DR_NO`, `Status`, `Status Desc`) VALUES (?,?,?);';
+//     db.query(sqlInsert, (err,result) => {
+//         response.send("Hello World");
+//     })
+// })
+
+app.post("/api/insert", (require, response) => {
+    const Status = require.body.Status;
+    const Status_Desc = require.body.Status_Desc;
+    const DR_NO = require.body.DR_NO;
+
+    console.log(DR_NO);
+
+    const sqlInsert = 'INSERT INTO `Status` (`DR_NO`, `Status`, `Status Desc`) VALUES (?,?,?)';
+    db.query(sqlInsert, [DR_NO, Status, Status_Desc], (err,result) => {
+        console.log(err);
+    })
+});
+
+app.get("/api/get", (require, response) => {
+    const sqlSelect = "SELECT * FROM Status";
+    db.query(sqlSelect, (err, result) => {
+        response.send(result);
+    });
+});
+
+app.delete("/api/delete/:DR_NO", (require, response) => {
+    // const Status = require.params.Status;
+    const DR_NO = require.params.DR_NO;
+
+    const sqlDelete = "DELETE FROM `Status` WHERE `DR_NO`=?;";
+    console.log('deleted');
+    db.query(sqlDelete, DR_NO, (err, result) => {
+        if(err)
+        console.log(err);
+    })
+});
+
+app.put("/api/update/", (require, response) => {
+    const Status = require.body.Status;
+    const Status_Desc = require.body.Status_Desc;
+    const DR_NO = require.body.DR_NO;
+
+    const sqlUpdate = "UPDATE `Status` SET `Status_Desc` = ? WHERE `DR_NO` = ?";
+    db.quire(sqlUpdate, [Status,Status_Desc,DR_NO], (err,result) => {
+        if(err)
+        console.log(err);
+    })
+});
 
 app.listen(3002, () => {
  console.log("running on port 3002");
