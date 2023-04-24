d620ca508badf2ab1ec79fc838e096e23fed26df
whhuang4 <123702462+whhuang4@users.noreply.github.com>
2023-04-24 23:36:59 +0000

    add hash function

---
+++ frontend/src/components/cryptoUtils.js
@@ -0,0 +1,25 @@
+// Mark the function as async to allow using await inside
+export async function hashPassword(password) {
+    // Convert the password to a Uint8Array
+    const encoder = new TextEncoder();
+    const data = encoder.encode(password);
+  
+    // Hash the password using the SubtleCrypto API
+    // crypto.subtle.digest returns a Promise, so we use await to wait for it to resolve
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+  
+    // Convert the hash to a hexadecimal string
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  
+    return hashHex;
+  }
+  
+  // Example usage
+  (async () => {
+    const password = 'YourSecurePassword';
+    // hashPassword is an async function, so we use await to get its result
+    const hashedPassword = await hashPassword(password);
+    console.log('Hashed password:', hashedPassword);
+  })();
+  
