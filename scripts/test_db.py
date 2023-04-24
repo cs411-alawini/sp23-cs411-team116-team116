138b1b1f6beee8437fc6b1b757e2d6dc1269e0ab
whhuang4 <123702462+whhuang4@users.noreply.github.com>
2023-04-24 23:37:42 +0000

    update support script

--- scripts/test_db.py
+++ scripts/test_db.py
@@ -12,20 +12,25 @@ cnx = mysql.connector.connect(**db_config)
 cursor = cnx.cursor()
 
 def test_store_procedure():
-    # Connect to the database
-
+    # Create a cursor.
+    cursor = cnx.cursor()
 
     # Set the input parameters
     user_id = 1
     latitude = 34.062265
     longitude = -118.246
     radius = 1000
-    query_id = 0
-    # Call the GetCrimeInfo stored procedure
-    result_args = cursor.callproc("GetCrimeInfo", [user_id, latitude, longitude, radius, query_id])
 
-    # Get the output parameter (query_id)
-    query_id = result_args[-1]
+    # Call the GetCrimeInfo stored procedure
+    query = "CALL GetCrimeInfo(%s, %s, %s, %s, @output_query_id);"
+    cursor.execute(query, (user_id, latitude, longitude, radius))
+
+    # Fetch the output parameter value.
+    cursor.execute("SELECT @output_query_id;")
+    query_id = cursor.fetchone()[0]
+
+    # Commit the changes
+    cnx.commit()
 
     # Get the query result
     cursor.execute("SELECT * FROM Query WHERE Query_ID = %s", (query_id,))
@@ -35,36 +40,9 @@ def test_store_procedure():
     for row in result:
         print(row)
 
-    # Execute the query
-    cursor.execute("SELECT IFNULL(MAX(Query_ID), 0) FROM Query")
-
-    # Fetch the result
-    result = cursor.fetchone()[0]
-
-    # Print the result
-    print(result)
-
-    query_id = 0
-    # Call the GetCrimeInfo stored procedure
-    result_args = cursor.callproc("GetCrimeInfo", [user_id, latitude, longitude, radius, query_id])
-
-    # Get the output parameter (query_id)
-    query_id = result_args[-1]
-
-    # Get the query result
-    cursor.execute("SELECT * FROM Query WHERE Query_ID = %s", (query_id,))
-    result = cursor.fetchall()
-
-    # Print the result
-    for row in result:
-        print(row)
-    # Get the query result
-    cursor.execute("SELECT * FROM Query")
-    result = cursor.fetchall()
-
-    # Print the result
-    for row in result:
-        print(row)
+    # Close the cursor and connection.
+    cursor.close()
+    cnx.close()
 
 
 def test_trigger():
@@ -114,8 +92,8 @@ def test_trigger():
         print(row)
 
 
-test_store_procedure()
-# test_trigger()
+# test_store_procedure()
+test_trigger()
 
 # cursor.close()
 # cnx.close()
