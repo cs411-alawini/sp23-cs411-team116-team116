9aecab18e139700724805e69ac0c4f9acf0746f6
whhuang4 <123702462+whhuang4@users.noreply.github.com>
2023-04-25 02:14:33 +0000

    complete backend

--- backend/index.js
+++ backend/index.js
@@ -24,24 +24,6 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-// CRUD example
-// app.post("/api/insert", (require, response) => {
-//     const movieName = require.body.movieName;
-//     const movieReview = require.body.movieReview;
-//     const sqlInsert = "INSERT INTO `movie_reviews` (`movieName`,`movieReview`) VALUES (?,?)";
-//     db.query(sqlInsert, [movieName, movieReview], (err, result) => {
-//     console.log(error);
-//     })
-//    });
-
-
-// app.get('/', (require,response) => {
-//     const sqlInsert = 'INSERT INTO `Status` (`DR_NO`, `Status`, `Status Desc`) VALUES (?,?,?);';
-//     db.query(sqlInsert, (err,result) => {
-//         response.send("Hello World");
-//     })
-// })
-
 // sidebar user function
 // send data by body
 app.post("/api/user/register", (require, response) => {
@@ -96,23 +78,26 @@ app.delete("/api/user/delete", (require, response) => {
 });
 
 // main page
-app.get("/api/mainpage/get", (require, response) => {
-    const query = 'CALL GetCrimeInfo(?, ?, ?, ?, @output_query_id)';
+// send data by header
+app.get("/api/mainpage/get", (req, response) => {
+    const user_name = req.headers.user_name;
+    const latitude = req.headers.lat;
+    const longitude = req.headers.lon;
+    const radius = req.headers.radius;
+    const callProcedure = 'CALL GetCrimeInfo(?, ?, ?, ?, @output_query_id)';
+    const sqlSelect =
+    db.query(callProcedure, [user_name, latitude, longitude, radius], (err, result) => {
+        if (err)
+            console.log(err);
+        console.log(result);
+        db.query('SELECT @output_query_id', (err, result) => {
+            if (err)
+                console.log(err);
+            console.log(result);
+            response.send(result);
+        });
+    });
 });
-// app.get("/api/search/by/status/:searchInput", (require, response) => {
-//     const searchInput = require.params.searchInput;
-//     const sqlSelect = "SELECT * FROM Status s WHERE s.Status= ? LIMIT 1000";
-//     console.log("search");
-//     db.query(sqlSelect, [searchInput], (err, result) => {
-//         console.log(result);
-//         // console.log(newList)
-//         response.send(result);
-//         if(err)
-//             console.log(err);
-//     });
-// });
-
-
 
 // Victims by Areas
 app.get("/api/area_victims_cnt/get", (require, response) => {
@@ -140,60 +125,75 @@ app.get("/api/weapon_victims_cnt/get", (require, response) => {
 
 // Query table
 app.get("/api/queryhistory/get", (require, response) => {
-
+    const sqlSelect = "SELECT * FROM `Query`";
+    console.log("/api/queryhistory/get");
+    db.query(sqlSelect, (err, result) => {
+        console.log(result);
+        response.send(result);
+        if(err)
+            console.log(err);
+    });
 });
 
 // Crime record CRUD page
 app.get("/api/crimedata/get", (require, response) => {
-
+    const sqlSelect = """SELECT * FROM `Victims`";
+    console.log("/api/crimedata/get");
+    db.query(sqlSelect, (err, result) => {
+        console.log(result);
+        response.send(result);
+        if(err)
+            console.log(err);
+    });
 });
 
 app.post("/api/crimedata/insert", (require, response) => {
-    const Status = require.body.Status;
-    const Status_Desc = require.body.Status_Desc;
     const DR_NO = require.body.DR_NO;
+    const Vict_Age = require.body.Vict_Age;
+    const Vict_Sex =  require.body.Vict_Sex;
+    const Weapon_Used_Cd = require.body.Weapon_Used_Cd;
+    const Crm_Cd =  require.body.Crm_Cd;
+    const AREA =  require.body.AREA;
+    const LAT =  require.body.LAT;
+    const LON =  require.body.LON;
 
     console.log("insert");
-    console.log(require.body);
 
-    const sqlInsert = 'INSERT INTO `Status` (`DR_NO`, `Status`, `Status Desc`) VALUES (?,?,?)';
-    db.query(sqlInsert, [DR_NO, Status, Status_Desc], (err,result) => {
+    const sqlInsert = 'INSERT INTO `Victims` (`DR_NO`, `Vict_Age`, `Vict_Sex`, `Weapon_Used_Cd`, `Crm_Cd`, `AREA`, `LAT`, `LON`) VALUES (?,?,?,?,?,?,?,?)';
+    db.query(sqlInsert, [DR_NO, Vict_Age, Vict_Sex, Weapon_Used_Cd, Crm_Cd, AREA, LAT, LON], (err,result) => {
         console.log(err);
     })
 });
 
 app.put("/api/crimedata/update", (require, response) => {
+    const DR_NO = require.body.DR_NO;
+    const Vict_Age = require.body.Vict_Age;
+    const Vict_Sex =  require.body.Vict_Sex;
+    const Weapon_Used_Cd = require.body.Weapon_Used_Cd;
+    const Crm_Cd =  require.body.Crm_Cd;
+    const AREA =  require.body.AREA;
+    const LAT =  require.body.LAT;
+    const LON =  require.body.LON;
 
+    console.log("update");
+
+    const updateQuery = "UPDATE `Victims` SET Vict_Age = ?, Vict_Sex = ?, Weapon_Used_Cd = ?, Crm_Cd = ?, AREA = ?, LAT = ?, LON = ? WHERE DR_NO = ?";
+    db.query(updateQuery, [Vict_Age, Vict_Sex, Weapon_Used_Cd, Crm_Cd, AREA, LAT, LON, DR_NO], (err,result) => {
+        if(err)
+        console.log(err);
+    })
 });
-// app.put("/api/update/", (require, response) => {
-//     // const Status = require.body.Status;
-//     const Status_Desc = require.body.Status_Desc;
-//     const DR_NO = require.body.DR_NO;
 
-//     console.log("update");
-//     console.log(require.body);
-
-//     const sqlUpdate = "UPDATE `Status` SET `Status Desc` = ? WHERE `DR_NO` = ?";
-//     db.query(sqlUpdate, [Status_Desc,DR_NO], (err,result) => {
-//         if(err)
-//         console.log(err);
-//     })
-// });
-
-app.delete("/api/crimedata/delete", (require, response) => {
+app.delete("/api/crimedata/delete/:DR_NO", (require, response) => {
+    const DR_NO = require.params.DR_NO;
+    console.log("delete");
+    const sqlDelete = "DELETE FROM Victims WHERE DR_NO=?;";
+    // console.log(DR_NO);
+    db.query(sqlDelete, DR_NO, (err, result) => {
+        if(err)
+        console.log(err);
+    })
 }); 
-// app.delete("/api/delete/:DR_NO", (require, response) => {
-//     // const Status = require.params.Status;
-//     const DR_NO = require.params.DR_NO;
-//     console.log("delete");
-//     console.log(require.params);
-//     const sqlDelete = "DELETE FROM `Status` WHERE `DR_NO`=?;";
-//     // console.log(DR_NO);
-//     db.query(sqlDelete, DR_NO, (err, result) => {
-//         if(err)
-//         console.log(err);
-//     })
-// });
 
 
 app.listen(3002, '0.0.0.0', () => {
